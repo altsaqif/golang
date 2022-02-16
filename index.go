79d099d849d451c0184a1f1996eaba643689b0ca
alsaa19 <99720129+altsaqif@users.noreply.github.com>
2022-02-16 12:13:15 +0700

    Adding object data types

--- index.go
+++ index.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("halo")
+	
+	var nama5 = [5]string{
+		"AlTsaqif", 
+		"Philips", 
+		"Ucup",
+		"Nugi",
+		"Indra",
+	}
+	nama5[2] = "Azka"
+	fmt.Println(nama5[4])
+	fmt.Println(nama5[2] == "Azka")
+	fmt.Println(nama5[2])
+	fmt.Println(nama5[0:4])
+	fmt.Println(nama5[0:])
+	fmt.Println(nama5[:4])
+	fmt.Println(nama5[:])
 }
